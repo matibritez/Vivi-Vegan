13f4320a4cce33567a0451f70d636c4a708df5d7
Marilina Roman <roman.marilinaf@gmail.com>
2022-10-17 21:47:07 -0300

    cards destacados

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 // cards con imagenes home
 
 const destacados = document.getElementById('productosDestacados')
-const divCards = document.createElement('div')
 
-let productos = [];
+let productos = []
 
 
 let myInit = {
@@ -18,29 +17,48 @@ let myInit = {
 const URL=new Request('../data/destacados.json', myInit)
 
 fetch(URL)
-    .then(function (response) {
-            return response.json();
-    }).then((json)=>{
-        Object(json).forEach(function(key){
-            productos.push(key);
+    .then((response)=>{
+        return response.json()
+    }).then((response)=>{
+        response.forEach(element => {
+            productos.push(element)
             return productos
         })
-        return productos
-    });
+    })
 
 console.log(productos);
 
+let cards = document.createElement('div')
+cards.className='cardsDestacados'
+let card = ''
+
+
+const crearCard = () =>{
+    if(productos!=undefined){
+        productos.forEach((e)=>{
+            card += `
+            <div class="tarjetas">
+                <div class="card" style="width: 15rem;">
+                    <img src=${e.url} class="card-img-top" alt="...">
+                </div>
+                <h5 class="card-title text-uppercase">${e.nombre}</h5>
+            </div>`
+
+            cards.innerHTML = card
+            destacados.appendChild(cards)
+        });
+
+    }
+}
+
+
+setTimeout(()=>{
+    crearCard()
+},2500)
 
 
 
-/*cards.innerHTML = productos.map((item)=>{
-        Object(json).forEach(function(key){
-            productos.push(key);
-            return productos
-        
-    })
 
-divCards.appendChild(cards)*/
 
 
 
